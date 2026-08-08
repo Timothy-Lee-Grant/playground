bc4e6fb9b3ea57da6c6d9dd5939f2a6d1496f534
Timothy-Lee-Grant <timothyleegrant1995@gmail.com>
2026-08-08 15:36:43 -0700

    my incorrect attempt

--- kafka/src/MyKafkaSystem.Producer/Producer.cs
+++ kafka/src/MyKafkaSystem.Producer/Producer.cs
@@ -1,18 +1,25 @@
 using Confluent.Kafka;
 using System.Net;
+using System.Reflection;
 
 namespace MyKafkaSystem.Producer;
 
 public class Producer(ILogger<Producer> logger) : BackgroundService
 {
+    private object _producer;
+    public Producer()
+    {
+        var _producer = new ProducerBuilder<NullabilityInfo, string>(config).Build();
+    }
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
             if (logger.IsEnabled(LogLevel.Information))
             {
-                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                logger.LogInformation("Producer running at: {time}", DateTimeOffset.Now);
             }
+            _producer.Produce("my-topic", new Message<Null, string> {Value = "hello world"}, handler)
             await Task.Delay(1000, stoppingToken);
         }
     }

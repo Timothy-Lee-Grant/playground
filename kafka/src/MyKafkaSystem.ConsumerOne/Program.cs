4993db9c5040f73d29b09b92459498f4d877bae1
Timothy-Lee-Grant <timothyleegrant1995@gmail.com>
2026-08-08 14:49:23 -0700

    finishing initial structure for kafka project

---
+++ kafka/src/MyKafkaSystem.ConsumerOne/Program.cs
@@ -0,0 +1,7 @@
+using MyKafkaSystem.ConsumerOne;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ kafka/src/MyKafkaSystem.ConsumerTwo/Program.cs
@@ -0,0 +1,7 @@
+using MyKafkaSystem.ConsumerTwo;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();

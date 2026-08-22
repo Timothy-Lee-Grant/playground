af735a0d9332561f38ec8f199505e1335fa8fb30
Timothy-Lee-Grant <timothyleegrant1995@gmail.com>
2026-08-22 15:15:05 -0700

    remembering basic c# user input

--- sqlite_interactions/Program.cs
+++ sqlite_interactions/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+// I want the user to be able to enter in their items that they want to put into the database
+// They should also be able to view items.
+while(true)
+{
+    string? userInput = Console.ReadLine();
+    Console.WriteLine($"You said: {userInput}");
+}

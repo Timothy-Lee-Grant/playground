ca69c88e74ed3d7301be91936962ac14a1c411b3
Timothy-Lee-Grant <timothyleegrant1995@gmail.com>
2026-08-22 15:41:05 -0700

    adding docs example

--- sqlite_interactions/Program.cs
+++ sqlite_interactions/Program.cs
@@ -1,7 +1,10 @@
 ﻿
+using Microsoft.Data.Sqlite;
+
 // I want the user to be able to enter in their items that they want to put into the database
 // They should also be able to view items.
-while(true)
+
+while (true)
 {
 
     Console.WriteLine("Select action:");
@@ -21,7 +24,24 @@ while(true)
 
 void AddValueToDataBase()
 {
-    
+    using var conn = new SqliteConnection("Data Source=testing123.db");
+    conn.Open();
+
+    using var command = conn.CreateCommand();
+    command.CommandText = """
+        Select name
+        From user
+        WHERE id = $id
+    """;
+
+    int id = 3;
+    command.Parameters.AddWithValue("$id", id);
+    using var reader = command.ExecuteReader();
+    while (reader.Read())
+    {
+        var name = reader.GetString(0);
+        Console.WriteLine($"Hello, {name}");
+    }
 }
 
 void ReadDataBase()
